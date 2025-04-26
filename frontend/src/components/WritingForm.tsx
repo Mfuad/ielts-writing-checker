aec3e86e9cb1bf269fa1b904ef1179db1d8a89ce
Mfuaddd <funt.004@mail.ru>
2025-04-27 00:07:41 +0400

    feat: update WritingForm
    add Text input, Title input, submit button, wordCounter function

--- frontend/src/components/WritingForm.tsx
+++ frontend/src/components/WritingForm.tsx
@@ -1,7 +1,93 @@
-function WritingForm() {
-  return (
-    <div>WritingForm</div>
-  )
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+
+const FormSchema = z.object({
+  title: z.string(),
+  text: z.string().refine((str) => isWordCountMin(str, 5), {
+    message: "Please enter at least 5 words",
+  }),
+});
+
+function wordCounter(string: string = "") {
+  const matches = string.match(/\S+/g);
+  return matches ? matches.length : 0;
 }
 
-export default WritingForm
+function isWordCountMin(string: string, count: number) {
+  return wordCounter(string) >= count;
+}
+
+interface IProps {
+  getScore: Function;
+}
+
+function WritingForm({ getScore }: IProps) {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  async function onSubmit(data: z.infer<typeof FormSchema>) {
+    getScore(data);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-xl">Title</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Tell us a little bit about yourself"
+                  className="resize-none"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="text"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-xl">Text</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Tell us a little bit about yourself"
+                  className="resize-none"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription>
+                Word Count: {wordCounter(field.value)}
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
+
+export default WritingForm;
