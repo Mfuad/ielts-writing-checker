dfee5d2671f81b6a8e067d82eb6fe8667d4c747b
Mfuaddd <funt.004@mail.ru>
2025-04-26 23:47:27 +0400

    feat: add MainPage page

---
+++ frontend/src/pages/MainPage.tsx
@@ -0,0 +1,12 @@
+import WritingForm from "@/components/WritingForm";
+
+function MainPage() {
+  return (
+    <>
+      <div>MainPage</div>
+      <WritingForm />
+    </>
+  );
+}
+
+export default MainPage;
